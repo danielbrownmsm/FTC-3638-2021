c2f3a8d86e3b7d879dd30fb3b6b353a22bad3be0
Daniel <56051347+danielbrownmsm@users.noreply.github.com>
2020-12-03 08:11:52 -0600

    Added arm_kP

--- Constants.java
+++ Constants.java
@@ -3,6 +3,8 @@ package org.firstinspires.ftc.teamcode;
 public static class Constants {
   public final double drive_kP = 0.05; // need to tune
   public final double turn_kP = 0.05; // need to tune
+  public final double arm_kP = 0.05; // need to tune
+  
   public final double headingThreshold = 0.1; // tune
   public final double TICKS_PER_REVOLUTION = 1; // change to actual value
   public final double WHEEL_DIAMETER = 4; // change to actual value
